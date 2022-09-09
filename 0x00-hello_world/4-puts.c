3b994d9818c97cbcf542b625e93678974b600146
Stanislauskaunda <stanislauskaunda@outlook.com>
2022-09-09 11:35:04 -0700

    4-puts.c'

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,13 @@
 /*
- *
- *  * File: 4-puts.c
- *
- *   * Auth: CharlyAnne
- *
- *    */
+ * File: 4-puts.c
+ * Auth: CharlyAnne
+ */
 #include <stdio.h>
 /**
- *
- *  * main- Print to screen.
- *
- *   *
- *
- *    * Return: Always 0 (success)
- *
- *     */
+ * main- Print to screen.
+ * 
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
