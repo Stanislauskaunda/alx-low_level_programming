418c20e45dfdf6656f26bf4ad156a1b4c35be69e
Stanislauskaunda <stanislauskaunda@outlook.com>
2022-09-09 11:49:10 -0700

    Put 4 c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,16 @@
 /*
- * File:4-put.c
- * Auth:CharlyAnne
+ * File: 4-put.c
+ * Auth: CharlyAnne
  */
-#include<stdio.h>
+#include <stdio.h>
+
 /**
- * main-print to screen.
+ * main- print to screen.
  *
- * Return:Always 0 (success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
 	put("\"Programming is like building a mutilingual puzzle");
-	return(0);
+	return (0);
 }
