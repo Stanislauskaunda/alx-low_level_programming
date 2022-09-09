6b549d32f5810a3a009a4fa3c44fa84f71ae1152
Stanislauskaunda <stanislauskaunda@outlook.com>
2022-09-09 12:30:44 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+/*
+ * File: 6-size.c
+ * auth: Stanislauskaunda
+ */
+/**
+ * main - print to screen
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long int a;
+	long long int b;
+	float f;
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(c));
+	printf("Size of an int: %zu byte(s)\n", sizeof(i));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(a));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(b));
+	printf("Size of a float: %zu byte(s)\n", sizeof(f));
+	return (0);
+}
