5426b91c797f53b0f9dacea347cb6a96bc62f0a5
Stanislauskaunda <stanislauskaunda@outlook.com>
2022-09-10 00:05:28 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+/*
+ * File - 101-quote.c
+ * Auth: Stanislauskaunda
+ */
+
+#include <unistd.h>
+/**
+ * main - Print to screen
+ *
+ * Retur: Always 1 (success)
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
