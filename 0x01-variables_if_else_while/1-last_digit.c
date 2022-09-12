1fc5b69e28fb7625dae7cfe3a13e1c3c8783f7c5
Stanislauskaunda <stanislauskaunda@outlook.com>
2022-09-12 11:40:09 -0700

    Fix  1-2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,6 +21,8 @@ int main(void)
 	else if (last_digit == 0)
 		printf("Last digit of %d is %d and is 0\n", n, last_digit);
 	else
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n"
+				, n, last_digit);
+
 	return (0);
 }
--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,12 +3,12 @@
 /**
  * main - print alphabet in lowercase
  *
- * return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char ch = 'a';
-	for (ch = 'a'; ch <= 'z'; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++) 
 		putchar(ch);
 	putchar('\n');
 	return (0);
