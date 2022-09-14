c6c17237f2ba4b2e4a665585e1adebb05a45db61
Stanislauskaunda <stanislauskaunda@outlook.com>
2022-09-14 13:14:11 -0700

    fine

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,11 +1,8 @@
 #include "main.h"
-
 /**
  * print_times_table - prints the n times table, starting with 0
- *
  * @n: number of the times table
  */
-
 void print_times_table(int n)
 {
 	int i, j, k;
@@ -25,14 +22,11 @@ void print_times_table(int n)
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
 					_putchar(k + '0');
 				}
 				else if (k >= 10 && k < 100)
 				{
 					_putchar(',');
-					_putchar(' ');
 					_putchar(' ');
 					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,15 +11,15 @@
 int main(void)
 {
 	int i;
-
+	
 	long a = 1, b = 2, r = 0;
+
 	printf("1, 2");
 	for (i = 1; i <= 48; i++)
 	{
 		r = a + b;
 		a = b;
 		b = r;
-
 		printf(", %ld", r);
 	}
 	putchar('\n');
