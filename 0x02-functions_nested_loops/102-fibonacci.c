15ce44c24302d5b1e39ba5392c5e6a5eabc1a326
Stanislauskaunda <stanislauskaunda@outlook.com>
2022-09-14 12:49:32 -0700

    Advaned

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Computes and prints sum of all the multiples of 3 or 5 below 1024
+ *
+ * i: Integer
+ * r: Result
+ * Return: on success 1.
+ */
+
+int main(void)
+{
+	int i;
+
+	long a = 1, b = 2, r = 0;
+	printf("1, 2");
+	for (i = 1; i <= 48; i++)
+	{
+		r = a + b;
+		a = b;
+		b = r;
+		printf(", %ld", r);
+	}
+	putchar('\n');
+	return (0);
+}
