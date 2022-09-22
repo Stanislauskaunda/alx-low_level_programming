d0eaabae4b3bfc97ec56efac4126784d3a5c33aa
Stanislauskaunda <stanislauskaunda@outlook.com>
2022-09-22 12:21:19 -0700

    fix 7

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,7 +3,7 @@
 /**
  * string_toupper - changes all lowercase letters of a string to uppercase
  * @s: input string.
- * new line, ,, ;, ., !, ?, , (, ), {, and }.
+ * new line, ,, ;, ., !, ?,' , (, ), {, and }.
  * @s: pointer to string.
  */
 char *cap_string(char *s)
@@ -22,7 +22,7 @@ char *cap_string(char *s)
 		if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
 				|| s[count] == ',' || s[count] == ';' || s[count] == '.'
 				|| s[count] == '.' || s[count] == '!' || s[count] == '?'
-				|| s[count] == '"' || s[count] == '(' || s[count] == ')'
+				|| s[count] == ''' || s[count] == '(' || s[count] == ')'
 				|| s[count] == '{' || s[count] == '}')
 		{
 			if (s[count + 1] >= 97 && s[count + 1] <= 122)
