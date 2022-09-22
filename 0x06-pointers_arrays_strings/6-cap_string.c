5123967efdac62dd59b1cd68144fd06e21adb873
Stanislauskaunda <stanislauskaunda@outlook.com>
2022-09-22 10:59:18 -0700

    fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,17 +3,37 @@
 /**
  * string_toupper - changes all lowercase letters of a string to uppercase
  * @s: input string.
- * Return: the pointer to dest.
+ * new line, ,, ;, ., !, ?, ", (, ), {, and }.
+ * @s: pointer to string.
  */
 
-char *string_toupper(char *s)
-{
-	int count = 0;
+char *cap_string(char *s)
 
-	while (*(s + count) != '\0')
+{
+	int count;
+
+	/* scan through string */
+
+	count = 0;
+
+	while (s[count] != '\0')
 	{
-		if ((*(s + count) >= 97) && (*(s + count) <= 122))
-			*(s + count) = *(s + count) - 32;
+		/* if next character after count is a char , capitalise it */
+		if (s[0] >= 97 && s[0] <= 122)
+		{
+			s[0] = s[0] - 32;
+		}
+		if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
+				|| s[count] == ',' || s[count] == ';' || s[count] == '.'
+				|| s[count] == '.' || s[count] == '!' || s[count] == '?'
+				|| s[count] == '"' || s[count] == '(' || s[count] == ')'
+				|| s[count] == '{' || s[count] == '}')
+		{
+			if (s[count + 1] >= 97 && s[count + 1] <= 122)
+			{
+				s[count + 1] = s[count + 1] - 32;
+			}
+		}
 		count++;
 	}
 	return (s);
