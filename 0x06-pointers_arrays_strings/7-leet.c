65a7392a3d2a94e37ee2a6681b29616de46dcb2a
Stanislauskaunda <stanislauskaunda@outlook.com>
2022-09-22 13:15:49 -0700

    7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,11 +6,12 @@
  * Return: the pointer to dest.
  */
 
-char *cap_string(char *s)
+char *leet(char *s)
 {
 	int count = 0, i;
 	int lower_case[] = {97, 101, 111, 116, 108};
 	int upper_case[] = {65, 69, 79, 84, 76};
+	int numbers[] = {52, 51, 48, 55, 49};
 
 	while (*(s + count) != '\0')
 	{
