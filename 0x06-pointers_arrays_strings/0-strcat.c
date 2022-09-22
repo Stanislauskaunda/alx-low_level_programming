0ed1aa4ddd471deff7388bf8caaa766492b227de
Stanislauskaunda <stanislauskaunda@outlook.com>
2022-09-22 07:24:24 -0700

    00fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,12 +14,12 @@ char *_strcat(char *dest, char *src)
 
 	i = 0;
 	j = 0;
-	
-	while (dest[i]!='\0')
+
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
-	while (scr[j]!='\0')
+	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
 		j++;
