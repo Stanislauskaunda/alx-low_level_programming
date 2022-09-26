ed383b3a5244f1a2bfe08a8ebb67ad59a1e5decf
Stanislauskaunda <stanislauskaunda@outlook.com>
2022-09-26 10:14:49 -0700

    FIx

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,9 +12,13 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
-	
-	for (i = 0; i < n; i++)
-		dest[i] = src[i];
+	unsigned int r = 0;
+	unsigned int i = n;
+
+	for (; r < i; r++)
+	{
+		dest[r] = src[r];
+		n--;
+	}
 	return (dest);
 }
