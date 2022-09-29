76e4de7bdf371b97d677818b2eb695e00cb598ed
Stanislauskaunda <stanislauskaunda@outlook.com>
2022-09-29 12:01:32 -0700

    fixed 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,27 +1,18 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - returns the natural square root of a number
- * @n: number to calculate the square root of
- * Return: the resulting square root
- 
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: value to raise
+ * @y: power
  *
+ * Return: result of the power
  */
 
-int actual_sqrt_recursion(int n, int i);
-
-int _sqrt_recursion(int n)
+int _pow_recursion(int x, int y)
 {
-	if (n < 0)
+	if (y < 0)
 		return (-1);
-	return (actual_sqrt_recursion(n, 0));
-}
-
-int actual_sqrt_recursion(int n, int i)
-{
-	if (i * i > n)
-		return (-1);
-	if (i * i == n)
-		return (i);
-	return (actual_sqrt_recursion(n, i + 1));
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
 }
